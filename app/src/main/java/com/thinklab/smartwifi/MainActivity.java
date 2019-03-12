f0594ddbee04e4eed565e0f58915025172dbc0ee
Nick Ivanov <nnrowan@gmail.com>
2019-03-12 06:33:53 -0500

    Add unversioned files

---
+++ app/src/main/java/com/thinklab/smartwifi/MainActivity.java
@@ -0,0 +1,47 @@
+package com.thinklab.smartwifi;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_hotspots:
+                    mTextMessage.setText(R.string.title_hotspots);
+                    return true;
+                case R.id.navigation_connection:
+                    mTextMessage.setText(R.string.title_connection);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+                case R.id.navigation_about:
+                    mTextMessage.setText(R.string.title_about);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
