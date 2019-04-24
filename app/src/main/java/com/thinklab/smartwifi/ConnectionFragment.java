810e8b3e7e4745916325966f9793042a53d44df9
Nick Ivanov <nnrowan@gmail.com>
2019-04-24 13:14:44 -0500

    New homepage.

--- app/src/main/java/com/thinklab/smartwifi/ConnectionFragment.java
+++ app/src/main/java/com/thinklab/smartwifi/ConnectionFragment.java
@@ -12,35 +12,53 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Chronometer;
 import android.widget.Button;
+import android.widget.Button;
 import android.os.SystemClock;
 
 public class ConnectionFragment extends Fragment {
     private TextView connectionBool;
     private TextView ipAddress;
     private TextView connectionTime;
-    private Button button;
+    private Button button1;
+
+    private Chronometer timetotalChrono;
+    private Chronometer simpleChronometer;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         View view = inflater.inflate(R.layout.fragment_connection, container, false);
-        connectionBool = (TextView) view.findViewById(R.id.connectionBool2Text);
-        button = (Button) view.findViewById(R.id.button);
-        ipAddress = (TextView) view.findViewById(R.id.ipText);
+
+
+
+        timetotalChrono = (Chronometer) view.findViewById(R.id.simpleChronometer1);
+        timetotalChrono.setBase(SystemClock.elapsedRealtime() - 1074000);
+
+        simpleChronometer = (Chronometer) view.findViewById(R.id.simpleChronometer);
+        simpleChronometer.setBase(SystemClock.elapsedRealtime() - 788000);
+
+        connectionBool = (TextView) view.findViewById(R.id.message12);
+        button1 = (Button) view.findViewById(R.id.button1);
+        ipAddress = (TextView) view.findViewById(R.id.message22);
         setRetainInstance(true);
 
-        button.setVisibility(View.GONE);
+        //button1.setVisibility();
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setConnectedStatus(false);
-                button.setVisibility(View.GONE);
+                button1.setVisibility(View.GONE);
                 setConnectionClock(false);
                 setIpAddress(false, "");
 
             }
         });
+
 
         return view;
     }
@@ -56,7 +74,7 @@ public class ConnectionFragment extends Fragment {
     }
 
     public void showDisconnect(boolean show){
-        button.setVisibility(View.VISIBLE);
+        button1.setVisibility(View.VISIBLE);
     }
 
     public void setIpAddress(boolean connected, String ip){
@@ -70,13 +88,12 @@ public class ConnectionFragment extends Fragment {
         }
     }
     public void setConnectionClock(boolean connected) {
-        Chronometer simpleChronometer = (Chronometer) getActivity().findViewById(R.id.simpleChronometer);
         if(connected){
-            simpleChronometer.setBase(SystemClock.elapsedRealtime());
+            //simpleChronometer.setBase(SystemClock.elapsedRealtime() - 788000);
             simpleChronometer.start();
         }
         else {
-            simpleChronometer.setBase(SystemClock.elapsedRealtime());
+            //simpleChronometer.setBase(SystemClock.elapsedRealtime() - 788000);
             simpleChronometer.stop();
         }
 
