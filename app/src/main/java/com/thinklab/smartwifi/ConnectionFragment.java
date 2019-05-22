e3ae237ff97e81f3b10169f5b9e44c40e488d3f7
max danley <huskerpackers@gmail.com>
2019-05-22 14:57:31 -0500

    Updated threads

--- app/src/main/java/com/thinklab/smartwifi/ConnectionFragment.java
+++ app/src/main/java/com/thinklab/smartwifi/ConnectionFragment.java
@@ -1,7 +1,6 @@
 package com.thinklab.smartwifi;
 
-import android.app.Activity;
-import android.content.Context;
+
 import android.graphics.Color;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
@@ -9,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Chronometer;
-import android.widget.Button;
 import android.widget.Button;
 import android.os.SystemClock;
 
@@ -60,16 +57,13 @@ import java.util.UUID;
 import static java.lang.String.valueOf;
 
 public class ConnectionFragment extends Fragment {
-    final Handler timerHandler = new Handler();
     public static ConnectionFragment C1;
     private TextView connectionBool;
     private TextView ipAddress;
-    private TextView connectionTime;
     private Button button1;
     private static TextView smartContractAddress;
     private static TextView connectionSpeed;
     private TextView etherWallet;
-    SWFHelper swfHelper = new SWFHelper();
     private static Chronometer timetotalChrono;
     private static Chronometer simpleChronometer;
     private TextView wifissid;
@@ -78,6 +72,13 @@ public class ConnectionFragment extends Fragment {
     private static FragmentActivity mInstance;
     private static long timeWhenStopped = 0;
     private static TextView smartStatus;
+    private static volatile boolean flag2 = true;
+    private static volatile boolean dupset_flag = true;
+    private static volatile boolean thread2 = true;
+    private static Thread thread;
+    private static Thread worker;
+    private static double speed_min = 2.0;
+    private static Socket socket;
 
 
     @Nullable
@@ -123,8 +124,18 @@ public class ConnectionFragment extends Fragment {
                 totalCharge.setText("0.000 ETH");
                 connectionSpeed.setText("N/A");
                 ipAddress.setText("N/A");
+                dupset_flag = false;
+                flag2 = false;
+                try{
+                    socket.close();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 //WifiManager wifiManager = (WifiManager) getContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
                 //wifiManager.setWifiEnabled(false);
+
 
 
             }
@@ -137,6 +148,8 @@ public class ConnectionFragment extends Fragment {
         if(connected) {
             connectionBool.setText("Active");
             connectionBool.setTextColor(Color.GREEN);
+            flag2 = true;
+
         }
         else{
             connectionBool.setText("Dissconnected");
@@ -149,6 +162,10 @@ public class ConnectionFragment extends Fragment {
         speed = speedTmp;
         Log.d("speed is ", valueOf(speed));
 
+    }
+
+    public static void setSpeed_min(Double speed_min2){
+        speed_min = speed_min2;
     }
 
     public void showDisconnect(boolean show){
@@ -666,9 +683,7 @@ public class ConnectionFragment extends Fragment {
             }
 
         }
-        static void dupsetProbe1(){
 
-        }
 
         public static void dupsetProbe() {
 
@@ -725,7 +740,7 @@ public class ConnectionFragment extends Fragment {
 
 
 
-            for(int k = 0; k < 120; k++) {
+            for(int k = 0; k < 120 && !dupset_flag; k++) {
 
                 sma2 = 0.0;
                 sma3 = 0.0;
@@ -926,7 +941,8 @@ public class ConnectionFragment extends Fragment {
                 }
 
                 System.out.println("SPEED_REPORT: " + tcma);
-                //SWFHelper.nbSleep(1000);
+
+                // SWFHelper.nbSleep(1000);
             }
         }
 
@@ -935,7 +951,7 @@ public class ConnectionFragment extends Fragment {
                 dupsetProbe();
             };
 
-            Thread worker = new Thread(task);
+            worker = new Thread(task);
             worker.start();
         }
 
@@ -952,7 +968,7 @@ public class ConnectionFragment extends Fragment {
 
 
     public static void run2() throws Exception {
-        Thread thread = new Thread(new Runnable() {
+        thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -961,7 +977,7 @@ public class ConnectionFragment extends Fragment {
                     //Log.d(SWFHelper.getFullHashChain("hello", 10).toString(), "");
                     //System.out.println(SWFHelper.getFullHashChain("hello", 10));
 
-                    ClientSecret secret = SWFHelper.generateClientSecret();
+                    ClientSecret secret = generateClientSecret();
 
                     //SWFLogger.log("hashchain", "secret class object", secret.toString());
 
@@ -985,7 +1001,12 @@ public class ConnectionFragment extends Fragment {
                     String server_host = "10.42.0.1";
                     Log.d("Attempting Socket", "now");
                     // [[ SEND: TRANSMISSION #0 ]]
-                    try (Socket socket = new Socket(server_host, Integer.parseInt(port))) {
+
+                    try {
+                        socket = new Socket(server_host, Integer.parseInt(port));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    } finally {
                         Scanner in = new Scanner(socket.getInputStream());
                         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
@@ -1040,223 +1061,13 @@ public class ConnectionFragment extends Fragment {
                             return;
                         }
 
-                        SWFHelper.contractFund(web3, wallet_public, contract, credentials, price);
+                        contractFund(web3, wallet_public, contract, credentials, price);
 
                         //String result = wallet.sendTransaction(web3, wallet_public, contract, credentials, price);
 
                         // [[ SEND: TRANSMISSION #4 ]]
                         out.println("OK");
-                        //Log.d("OK", "");
 
-
-                        // MESSAROUND
-
-
-//            List<Type> inputParameters = new ArrayList<>();
-//            List<TypeReference<?>> outputParameters = new ArrayList<>();
-//
-//
-//            outputParameters.add(new TypeReference<Utf8String>() {});
-//
-//            Function function = new Function("renderHelloWorld",
-//                    inputParameters,
-//                    outputParameters);
-//            String functionEncoder = FunctionEncoder.encode(function);
-//
-//            EthCall response = web3.ethCall(Transaction.createEthCallTransaction("0xe57EB9F6f71B4f9F37e6aB730942851d067d402E","0x03f081e292a6645d5bbd2017ca035175cd6a0b05", functionEncoder), DefaultBlockParameterName.LATEST).sendAsync().get();
-//
-////            Type resault = someType.get(0);
-////
-////            String a = resault.toString();
-//
-//
-//
-//            List<Type> someType = FunctionReturnDecoder.decode(response.getValue(),function.getOutputParameters());
-//            Iterator<Type> it = someType.iterator();
-//            Type resault = someType.get(0);
-//            String a = resault.toString();
-//
-//            System.out.println("Hello0: " + a);
-//            System.out.println("Hello0.1: " + response.toString());
-//            System.out.println("Hello1: " + response.getValue());
-//            System.out.println("Hello2: " + response.getResult());
-//            System.out.println("Hello3: " + response.getRawResponse());
-
-
-                        // END-OF-MESSAROUND
-
-
-                        // MESSAROUND 1
-
-//            System.out.println("blah: " + secret.getTop().replaceAll("0x", ""));
-//
-//
-//
-//
-//            List<Type> inputParameters = new ArrayList<>();
-//            inputParameters.add(new Bytes32(Numeric.hexStringToByteArray(secret.getTop().replaceAll("0x", ""))));
-//
-//
-//            List<TypeReference<?>> outputParameters = new ArrayList<>();
-//
-//
-//            //outputParameters.add(new TypeReference<Utf8String>() {});
-//
-//            Function function = new Function("setHash",
-//                    inputParameters,
-//                    outputParameters);
-//            String functionEncoder = FunctionEncoder.encode(function);
-//
-//            EthCall response = web3.ethCall(Transaction.createEthCallTransaction("0xe57EB9F6f71B4f9F37e6aB730942851d067d402E","0xfbb8784d5980c19ce5c9e2d7fcc793b3176b1718", functionEncoder), DefaultBlockParameterName.LATEST).sendAsync().get();
-//
-////            Type resault = someType.get(0);
-////
-////            String a = resault.toString();
-//
-//
-//
-//            //List<Type> someType = FunctionReturnDecoder.decode(response.getValue(),function.getOutputParameters());
-//            //Iterator<Type> it = someType.iterator();
-//            //Type resault = someType.get(0);
-//            //String a = resault.toString();
-//
-//
-//            //This guy: 7e5fd2767e1a8499bfc8672ef9c9d7d0265c5a965978ee7bed78ea705f44b11b
-//
-//            //System.out.println("Hello0: " + a);
-//            System.out.println("Hello0.1: " + response.toString());
-//            System.out.println("Hello1: " + response.getValue());
-//            System.out.println("Hello2: " + response.getResult());
-//            System.out.println("Hello3: " + response.getRawResponse());
-
-                        // END OF MESSAROUND 1
-
-
-                        // MESSAROUND 1.5
-
-//            System.out.println("top: " + secret.getTop());
-//
-//            List<Type> inputParameters = new ArrayList<>();
-//            inputParameters.add(new Bytes32(Numeric.hexStringToByteArray(secret.getTop().replaceAll("0x", ""))));
-//
-//
-//            List<TypeReference<?>> outputParameters = new ArrayList<>();
-//
-//
-//            //outputParameters.add(new TypeReference<Utf8String>() {});
-//
-//            Function function = new Function("setHash",
-//                    inputParameters,
-//                    outputParameters);
-//
-//            String encodedFunction = FunctionEncoder.encode(function);
-//
-//            BigInteger gasPrice = BigInteger.valueOf(200000000000L);
-//            BigInteger gasLimit = BigInteger.valueOf(5000000L);
-//            //BigInteger nonce = null;
-//            EthGetTransactionCount ethGetTransactionCount = web3.ethGetTransactionCount(
-//                    "0xe57EB9F6f71B4f9F37e6aB730942851d067d402E", DefaultBlockParameterName.LATEST).send();
-//            BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-//
-//
-//            Transaction transaction = Transaction.createFunctionCallTransaction(
-//                    "0xe57EB9F6f71B4f9F37e6aB730942851d067d402E", nonce, gasPrice, gasLimit, "0xfbb8784d5980c19ce5c9e2d7fcc793b3176b1718", encodedFunction);
-//
-//
-//            RawTransaction rawTransaction  = RawTransaction.createTransaction(
-//                    nonce, gasPrice, gasLimit, "0xfbb8784d5980c19ce5c9e2d7fcc793b3176b1718", encodedFunction);
-//
-//            //org.web3j.protocol.core.methods.response.EthSendTransaction transactionResponse =
-//              //      web3.ethSendTransaction(transaction).sendAsync().get();
-//
-//            System.out.println("transaction: " + rawTransaction.getData());
-//            //System.out.println("transaction: " + rawTransaction.);
-//            System.out.println("transaction: " + rawTransaction.toString());
-//            //System.out.println("transaction: " + rawTransaction.getData());
-//
-//
-//            if(rawTransaction == null) {
-//                System.out.println("raw transaction is null");
-//            }
-//
-//            byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
-//            String hexValue = Numeric.toHexString(signedMessage);
-//            EthSendTransaction rawTrasactionResponse = web3.ethSendRawTransaction(hexValue).send();
-//
-//
-//            //org.web3j.protocol.core.methods.response.EthSendTransaction rawTrasactionResponse = web3.ethSendRawTransaction("0x" + rawTransaction.getData()).sendAsync().get();
-//
-//            //String transactionHash = transactionResponse.getTransactionHash();
-//
-//
-//            String rawTransactionHash = rawTrasactionResponse.getTransactionHash();
-//
-//            System.out.println("Transaction hash: " + rawTransactionHash);
-//            System.out.println("tr.toString(): " + rawTrasactionResponse.toString());
-//            System.out.println("tr.getJsonrpc(): " + rawTrasactionResponse.getJsonrpc());
-//            System.out.println("tr.getRawResponse(): " + rawTrasactionResponse.getRawResponse());
-//            System.out.println("tr.getResult(): " + rawTrasactionResponse.getResult());
-//            System.out.println("tr.hasError(): " + rawTrasactionResponse.hasError());
-//            //System.out.println("tr.getError(): " + rawTrasactionResponse.getError().getMessage());
-//
-//
-////            System.out.println("Transaction hash: " + transactionHash);
-////            System.out.println("tr.toString(): " + transactionResponse.toString());
-////            System.out.println("tr.getJsonrpc(): " + transactionResponse.getJsonrpc());
-////            System.out.println("tr.getRawResponse(): " + transactionResponse.getRawResponse());
-////            System.out.println("tr.getResult(): " + transactionResponse.getResult());
-////            System.out.println("tr.hasError(): " + transactionResponse.hasError());
-////            System.out.println("tr.getError(): " + transactionResponse.getError().getMessage());
-
-
-                        //SWFHelper.contractSetHash(web3, credentials, "0xe57EB9F6f71B4f9F37e6aB730942851d067d402E", "0xfbb8784d5980c19ce5c9e2d7fcc793b3176b1718", secret.getTop());
-
-                        //System.out.println("hash setting: " + secret.getTop());
-
-                        //Thread.sleep(60000);
-
-
-                        //System.out.println("hash returned: " + SWFHelper.contractGetHash(web3, "0xe57EB9F6f71B4f9F37e6aB730942851d067d402E", "0xfbb8784d5980c19ce5c9e2d7fcc793b3176b1718"));
-
-
-                        // END OF MESSAROUND 1.5
-
-
-                        // MESSAROUND 2
-
-//            List<Type> inputParameters1 = new ArrayList<>();
-//            List<TypeReference<?>> outputParameters1 = new ArrayList<>();
-//
-//
-//            outputParameters1.add(new TypeReference<Bytes32>() {});
-//
-//            Function function1 = new Function("getHash",
-//                    inputParameters1,
-//                    outputParameters1);
-//            String functionEncoder1 = FunctionEncoder.encode(function1);
-//
-//            EthCall response = web3.ethCall(Transaction.createEthCallTransaction("0xe57EB9F6f71B4f9F37e6aB730942851d067d402E","0xfbb8784d5980c19ce5c9e2d7fcc793b3176b1718", functionEncoder1), DefaultBlockParameterName.LATEST).sendAsync().get();
-//
-////            Type resault = someType.get(0);
-////
-////            String a = resault.toString();
-//
-//
-//
-//            List<Type> someType = FunctionReturnDecoder.decode(response.getValue(),function1.getOutputParameters());
-//            Iterator<Type> it = someType.iterator();
-//            Type resault = someType.get(0);
-//            Bytes32 a = (Bytes32) resault;
-//
-//
-//            System.out.println("Hello0: " + Numeric.toHexString(((Bytes32) resault).getValue()));
-//            System.out.println("Hello0.0.1: " + Numeric.toHexString(a.getValue()) );
-//            System.out.println("Hello0.1: " + response.toString());
-//            System.out.println("Hello1: " + response.getValue());
-//            System.out.println("Hello2: " + response.getResult());
-//            System.out.println("Hello3: " + response.getRawResponse());
-
-                        // END OF MESSAROUND 2
 
                         try {
                             // [[ RECEIVE: TRANSMISSION #5 ]]
@@ -1300,20 +1111,25 @@ public class ConnectionFragment extends Fragment {
                         int chainsize = 60;
                         int subperiod_msec = 60000;
 
-                        ArrayList<String> fullHashChain = SWFHelper.getFullHashChain(secret.getSeed(), 60);
+                        ArrayList<String> fullHashChain = getFullHashChain(secret.getSeed(), 60);
                         Collections.reverse(fullHashChain);
 
 
                         ConnectionFragment.startDupset();
 
-                        SWFHelper.nbSleep(10000);
 
-                        double speed_min = 2.0;
+                         nbSleep(10000);
+
 
                         int testdrive_periods = 2;
 
                         int count = 0;
-                        for (int i = 0; i < chainsize * 2; i++) {
+
+
+                        for (int i = 0; i < chainsize * 2 && flag2 ; i++) {
+                            if(!thread2){
+                                thread.interrupt();
+                            }
 
                             if (count >= chainsize) {
                                 break;
@@ -1370,7 +1186,7 @@ public class ConnectionFragment extends Fragment {
                     SWFHelper.nbSleep(subperiod_msec * 2);
                 } */
 
-                            SWFHelper.nbSleep(subperiod_msec);
+                            nbSleep(subperiod_msec);
                         }
 
                         try {
@@ -1379,7 +1195,7 @@ public class ConnectionFragment extends Fragment {
                             ex.printStackTrace();
                         }
 
-                        SWFHelper.contractRefund(web3, credentials, wallet_public, contract, secret.getTop());
+                        contractRefund(web3, credentials, wallet_public, contract, secret.getTop());
                     }
 
                 }catch(Exception e){
